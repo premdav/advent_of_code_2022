278fa70cf877737eb5aa0e5ec0a9d6befa2e5552
Preston Davis <prem.dav@outlook.com>
2022-12-02 20:36:50 -0600

    Part two tweaks

--- day_two/strategyScore.js
+++ day_two/strategyScore.js
@@ -1,42 +1,47 @@
 // input - matchup and mine's selections for rock paper scissors
-// a, x = rock | b, y = paper | c, z = scissors
+// a = rock | b = paper | c = scissors
+// X = need to lose, Y = need to draw, Z = need to win
 // rock > scissors; paper > rock; scissors > paper;
 // rock = 1 point, paper = 2 points, scissors = 3 points
 // loss = 0 points, draw = 3 points, win = 6 points
+// determine the shape you need to play and then
 // figure out how many points you will have by following the strategy guide exactly
 const fs = require('fs');
 
 const allLines = fs.readFileSync('input.txt', 'utf-8');
-const selectionPoints = {
-    X: 1,
-    Y: 2,
-    Z: 3,
+const outcomePoints = {
+    X: 0,
+    Y: 3,
+    Z: 6,
 };
+
+// selection map will determine the points of my selection needed to 
+// make the correct outcome possible based on need to win, draw, or lose
 const selectionMap = {
     A: {
-        X: 3,
-        Y: 6,
-        Z: 0,
+        X: 3, // lose
+        Y: 1, // draw
+        Z: 2, // win
     },
     B: {
-        X: 0,
-        Y: 3,
-        Z: 6,
+        X: 1,
+        Y: 2,
+        Z: 3,
     },
     C: {
-        X: 6,
-        Y: 0,
-        Z: 3,
+        X: 2,
+        Y: 3,
+        Z: 1,
     },
 };
 let totalScore = 0;
 allLines.split(/\r?\n/).forEach((line) => {
     const selections = line.split(' ');
-    const them = selections[0], me = selections[1];
+    const them = selections[0], outcome = selections[1];
+    // adds points for needed outcome
+    totalScore += outcomePoints[outcome];
     // adds points for my selection
-    totalScore += selectionPoints[me];
-    // adds points for the win/loss/draw
-    totalScore += selectionMap[them][me];
+    totalScore += selectionMap[them][outcome];
 });
 
 ////////////////////////////////////////////////////////////////////////////////
