7f66342040dc9f67ebb4d70c881616459a5ff2f0
Preston Davis <prem.dav@outlook.com>
2022-12-04 10:19:42 -0600

    Part two solution

--- day_four/campCleanup.js
+++ day_four/campCleanup.js
@@ -1,19 +1,18 @@
 // elves in pairs - each assigned a range of sections for cleaning camp -> 2-3, 5-8
-// in how many assignment pairs does one range fully contain the other?
+// in how many assignment pairs have overlap?
 const fs = require('fs');
 const lines = fs.readFileSync('input.txt', 'utf-8').split(/\r?\n/);
-let pairsWithContainment = 0;
+let pairsWithOverlap = 0;
 for (line of lines) {
     const firstPair = line.split(',')[0].split('-'), secondPair = line.split(',')[1].split('-');
-    // checking if first pair fully contains second
-    if ((parseInt(firstPair[0], 10) >= parseInt(secondPair[0], 10)) && (parseInt(firstPair[1], 10) <= parseInt(secondPair[1], 10))) {
-        pairsWithContainment += 1;
-    } else if ((parseInt(secondPair[0], 10) >= parseInt(firstPair[0], 10)) && (parseInt(secondPair[1], 10) <= parseInt(firstPair[1], 10))) {
-        pairsWithContainment += 1;
+    if ((parseInt(secondPair[0], 10) <= parseInt(firstPair[0], 10)) && (parseInt(firstPair[0], 10) <= parseInt(secondPair[1], 10))) {
+        pairsWithOverlap += 1;
+    } else if ((parseInt(firstPair[0], 10) <= parseInt(secondPair[0], 10)) && (parseInt(secondPair[0], 10) <= parseInt(firstPair[1], 10))) {
+        pairsWithOverlap += 1;
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 ///// printing results to paste into answer on website                     /////
 ////////////////////////////////////////////////////////////////////////////////
-console.log('assignment pairs with one range fully contained', pairsWithContainment);
+console.log('assignment pairs with any overlap', pairsWithOverlap);
