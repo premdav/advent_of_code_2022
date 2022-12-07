c7fe12504b704b78acc0aa57a0c439a7f27c20ea
Preston Davis <prem.dav@outlook.com>
2022-12-06 19:44:54 -0600

    Part one solution

---
+++ day_6/tuningTrouble.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt', 'utf-8').split(/\r?\n/)[0];
+
+const dataStream = [];
+let packetMarker = 0;
+
+for (let i = 0; i < input.length; i += 1) {
+    if (dataStream.length === 4) {
+        const dataSet = new Set(dataStream);
+        if (dataSet.size === 4) {
+            packetMarker = i;
+            break;
+        }
+        dataStream.shift();
+    }
+    dataStream.push(input[i]);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///// printing results to paste into answer on website                     /////
+////////////////////////////////////////////////////////////////////////////////
+console.log('packet marker:', packetMarker);
